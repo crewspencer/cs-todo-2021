5a2ba79bc5bab123125303e982324e4927b4c5f3
Titus . Roman <titus@CIT-SCC112-1107.cs.dixie.edu>
2021-06-24 10:32:07 -0600

    fixed index port

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const server = require("./server");
 const persist = require("./persist");
 
 // define a port
-const PORT = process.env.PORT || 3000;
-
+const portNumber = process.argv[2] || process.env.PORT || 3000;
 
 // connect to the database
 persist.connect(function () {
   // start the server
   server.listen(portNumber, () => {
-    console.log(`Running Server on Port ${PORT}`);
+    console.log(`Running Server on Port ${portNumber}`);
   });
 });
